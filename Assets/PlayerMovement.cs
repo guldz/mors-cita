e3ba35e98cca5808ca320b8ec54dbffedb0b411b
hugo <sspel25hula@axevalla.fhsk.se>
2026-02-17 16:01:52 +0100

    enemy and pathfinding

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,26 +3,51 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public int playerHealth = 1;
+    public float moveSpeed = 5f;
+    Rigidbody2D rb;
+    public bool isMoving => rb.linearVelocity.magnitude > 0.1f;
 
-    
-    void Start()
-    {
-        
-    }
+    private Vector2 moveInput;
 
-    
     void Update()
     {
+        GetMovementInput();
+        Move();
         LookAtMouse();
-        
+    }
 
+    private void GetMovementInput()
+    {
+
+        moveInput = Keyboard.current.wKey.isPressed ? Vector2.up : Vector2.zero;
+        if (Keyboard.current.sKey.isPressed) moveInput += Vector2.down;
+        if (Keyboard.current.aKey.isPressed) moveInput += Vector2.left;
+        if (Keyboard.current.dKey.isPressed) moveInput += Vector2.right;
+
+        moveInput = moveInput.normalized;
+    }
+
+    private void Move()
+    {
+        transform.position += (Vector3)(moveInput * moveSpeed * Time.deltaTime);
     }
 
     private void LookAtMouse()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()); //current position type shit idiot!
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         transform.up = (Vector3)(mousePos - new Vector2(transform.position.x, transform.position.y));
     }
 
- 
+    public void TakingDamage(int damageTaken)
+    {
+        playerHealth = playerHealth - damageTaken;
+        if (playerHealth <= 0)
+        {
+            Destroy(gameObject);
+
+        }
+
+    }
 }
+
