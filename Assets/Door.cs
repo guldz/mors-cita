22724dea55378f0a833e03e650be1fa76b4e097e
hugo <sspel25hula@axevalla.fhsk.se>
2026-03-17 16:10:07 +0100

    doors are hard to make

---
+++ Assets/Door.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Animator doorAnimator;
+    public Collider2D doorCollider;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorAnimator.SetTrigger("Open");
+            doorCollider.enabled = false; // Now enemies can path through
+        }
+    }
+}
+
+
